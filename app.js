e48a7f6ae3ee560d0faa11943794eacc352a3b83
John Idenyi <johnidenyi2001@gmail.com>
2024-02-06 12:01:31 +0100

    Fixed error function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("loan-form");
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
   // Hide results
   document.getElementById("results").style.display = "none";
 
@@ -23,24 +23,23 @@ function calculateResults() {
   const totalInterest = document.getElementById("total-interest");
 
   const principal = parseFloat(amount.value);
-  const calculatedInterest = parseFloat(interestRate.value) / 100 /12;
+  const calculatedInterest = parseFloat(interestRate.value) / 100 / 12;
   const calculatedPayments = parseFloat(years.value) * 12;
 
   // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal * x * calculatedInterest)/(x-1);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
   if (isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
 
     // Show results
     document.getElementById("results").style.display = "block";
 
     // Hide loader
     document.getElementById("loading").style.display = "none";
-    
   } else {
     showError("Please check your numbers");
   }
@@ -48,15 +47,15 @@ function calculateResults() {
 
 // Show Error
 function showError(error) {
-   // Hide results
-   document.getElementById("results").style.display = "none";
-    
-   // Hide loader
-   document.getElementById("loading").style.display = "none";
+  // Hide results
+  document.getElementById("results").style.display = "none";
+
+  // Hide loader
+  document.getElementById("loading").style.display = "none";
 
   // Get elements
   const card = document.querySelector(".card");
-  const heading =document.querySelector(".heading");
+  const heading = document.querySelector(".heading");
 
   // Create a div
   const errorDiv = document.createElement("div");
@@ -76,7 +75,5 @@ function showError(error) {
 
 // Clear error
 function clearError() {
-  // document.querySelector(".alert").remove();
+  document.querySelector(".alert").remove();
 }
-
-
